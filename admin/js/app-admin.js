9113c8c634f71f682b09f86eb877f9ee12f2b4bc
wilsonriosv <wilsonriosv@gmail.com>
2023-05-08 18:28:37 -0500

    Ajustes de código...
    Ya con estos ajustes queda listo el carrito de compras.
    Queda pendiente realizar filtros de productos, de precios, de categorías, etc.

--- admin/js/app-admin.js
+++ admin/js/app-admin.js
@@ -11,6 +11,8 @@ function obtenerAlmacenamientoLocal(llave){
 
 let productos = obtenerAlmacenamientoLocal('productos') || [];
 let mensaje = document.getElementById('mensaje');
+let ruta = window.location.pathname;
+console.log(ruta);
 
 /* Añadir productos */
 const anadirProducto = document.getElementById('productoAnadir');
@@ -50,7 +52,7 @@ document.getElementById('botonAnadir').addEventListener('click', function(ev){
         })
         mensaje.classList.add('realizado');
         setTimeout(() => {
-            mensaje.classList.remove('repetidoError');
+            mensaje.classList.remove('realizado');
             window.location.reload();
         }, 1500)
     }
@@ -59,48 +61,106 @@ document.getElementById('botonAnadir').addEventListener('click', function(ev){
 });
 
 /* Editar productos */
-const productoEditar = document.getElementById('productoEditar');
-const atributoEditar = document.getElementById('atributoEditar');
-const nuevoAtributoEditar = document.getElementById('nuevoAtributo');
+const editarProducto = document.getElementById('productoEditar');
+const editarAtributo = document.getElementById('atributoEditar');
+const editarNuevoAtributo = document.getElementById('nuevoAtributo');
 
 document.getElementById('botonEditar').addEventListener('click', (ev) => {
-    let productoEditar = productoEditar.value;
-    let atributoEditar = atributoEditar.value;
-    let nuevoAtributoEditar = nuevoAtributoEditar.value;
+    ev.preventDefault();
+    let productoEditar = editarProducto.value;
+    let atributoEditar = editarAtributo.value;
+    let nuevoAtributoEditar = editarNuevoAtributo.value;
 
-    let sw = true;
+    let sw = false;
     if (productoEditar == '' || atributoEditar == '' || nuevoAtributoEditar == '' ){
         mensaje.classList.add('llenarCampos');
         setTimeout(() => { mensaje.classList.remove('llenarCampos')},2500)
-        sw = false;
     }else{
         for (let i = 0; i < productos.length; i++) {
             if (productos[i].nombre == productoEditar) {
-                switch (atributoEditar) {
-                    case 'nombre':
-                        productos[i].nombre = nuevoAtributoEditar
-                        break;
-                    case 'valor':
-                        productos[i].valor = nuevoAtributoEditar
-                        break;
-                    case 'existencia':
-                        productos[i].existencia = nuevoAtributoEditar
-                        break;
-                    case 'urlImagen':
-                        productos[i].urlImagen = nuevoAtributoEditar
-                        break;
-                    default:
-                        break;
-                }
-                mensaje.classList.add('realizado');
-                setTimeout(() => {
-                    mensaje.classList.remove('repetidoError');
-                    window.location.reload();
-                }, 1500)
+                productos[i][atributoEditar] = nuevoAtributoEditar
+                sw=true;
             } 
         }
+        if (sw) {
+            mensaje.classList.add('realizado');
+            setTimeout(() => {
+                mensaje.classList.remove('realizado');
+                window.location.reload();
+            }, 1500)
+        }else{
+            mensaje.classList.add('noExisteError');
+            setTimeout(() => {mensaje.classList.remove('noExisteError') }, 2500);
+        }
+        guardarAlmacenamientoLocal('productos', productos);
+        console.log('Producto almacenado: ' + JSON.stringify(productos));
     }
-    guardarAlmacenamientoLocal('productos', productos);
+})
+
+/* Eliminar productos */
+const eliminarProducto = document.getElementById('productoEliminar');
+document.getElementById('botonEliminar').addEventListener('click', (ev) => {
+    ev.preventDefault();
+    let productoEliminar = eliminarProducto.value;
+    let sw=false;
+    if (productoEliminar == '') {
+        mensaje.classList.add('llenarCampos');
+        setTimeout(() => { mensaje.classList.remove('llenarCampos')},2500)
+    }else{
+        for (let i = 0; i < productos.length; i++) {
+            console.log(productos[i].nombre +'=='+ productoEliminar);
+            if (productos[i].nombre == productoEliminar) {
+                productos.splice(i, 1); 
+                sw=true;
+            } 
+        }
+        if (sw) {
+            mensaje.classList.add('realizado');
+            setTimeout(() => {
+                mensaje.classList.remove('realizado');
+                window.location.reload();
+            }, 1500)
+        }else{
+            mensaje.classList.add('noExisteError');
+            setTimeout(() => {mensaje.classList.remove('noExisteError') }, 2500);
+        }
+        guardarAlmacenamientoLocal('productos', productos);
+        console.log('Producto almacenado: ' + JSON.stringify(productos));
+    }
+
+})
+
+/* Mostrar todos los productos */
+window.addEventListener('load', () => {
+    /* Llenar los elementos de lista select del html */
+    const productoEd = document.getElementById('productoEditar');
+    const productoEl = document.getElementById('productoEliminar');
+    const atributoEd = document.getElementById('atributoEditar');
+    for (let i = 0; i < productos.length; i++) {
+        productoEd.innerHTML += `<option>${productos[i].nombre}</option>`;
+        productoEl.innerHTML += `<option>${productos[i].nombre}</option>`;
+    }
+    console.log("Llena los select");
+    /* Llenar el elemento de lista select del html de la lista de atributos*/
+    Object.keys(productos[0]).forEach(element => {
+        atributoEd.innerHTML += `<option>${element}</option>`;
+        console.log("Atributo: " + element);
+    });
     console.log('Producto almacenado: ' + JSON.stringify(productos));
-    
+    /* Llenar el div mostrarProductos del html */
+    let mostrarProductos = document.getElementById('mostrarProductos');
+    mostrarProductos.innerHTML = '';
+    for (let i = 0; i < productos.length; i++) {
+        mostrarProductos.innerHTML += `<div class="contenedorProductos"><img src="${productos[i].urlImagen}"<div class="informacion"><p>${productos[i].nombre}</p><p class="precio"><span>Precio: ${productos[i].valor}$</span></p><p> Existencia: ${productos[i].existencia}</p></div></div>`
+        /* 
+        <div>
+            <img src="../img/product1.jpg" alt="Producto 1">
+            <div class="informacion">
+                <p>Producto 1</p>
+                <p class="precio">$199<span>.99</span></p>
+                <button>Comprar</button>
+            </div>
+        </div>
+        */
+    }
 })
--- usuario/js/app.js
+++ usuario/js/app.js
@@ -1,7 +1,30 @@
+/* Configurar el almacenamiento local */
+function guardarAlmacenamientoLocal(llave, valor_a_guardar){
+    localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
+}
+/* Recuperar los datos almacenados */
+function obtenerAlmacenamientoLocal(llave){
+    const datos = JSON.parse(localStorage.getItem(llave))
+    return datos
+}
+let productos = obtenerAlmacenamientoLocal('productos') || [];
+/* Variables que traemos de nuestro HTML */
+const infoCompra = document.getElementById('infoCompra');
+const contenedorCompra = document.getElementById('contenedorCompra');
+const productosCompra = document.getElementById('productosCompra');
+const contenedor = document.getElementById('contenedor');
+const carrito = document.getElementById('carrito');
+const numero = document.getElementById('numero');
 const header = document.querySelector("#header")
-const contenedor = document.querySelector('#contenedor');
+const total = document.getElementById("total");
 const body = document.querySelector('body');
+const x = document.getElementById('x');
 
+/* Variables que vamos a usar en nuestro proyecto */
+let lista = [];
+let valorTotal = 0;
+
+/* Scroll de nuestra página */
 window.addEventListener('scroll', function() {
     if(contenedor.getBoundingClientRect().top < 10){
         header.classList.add('scroll');
@@ -10,3 +33,96 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll');
     }
 })
+
+/* Cauando cargue la página */
+window.addEventListener('load', () => {
+    visualizarProductos();
+    contenedorCompra.classList.add('none');
+})
+
+/* Función para .... */
+function visualizarProductos() {
+    contenedor.innerHTML = "";
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].existencia > 0) {
+            contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><button onclick=comprar(${i})>Comprar</button></div></div>`
+        }else{
+            contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><p class="soldOut">Sold Out</p></div></div>`
+        }
+    }
+}
+
+/* Función para ... */
+function comprar(indice) {
+    lista.push({nombre: productos[indice].nombre, precio: productos[indice].valor})
+    let sw = true;
+    let i = 0;
+    while (sw) {
+        if (productos[i].nombre == productos[indice].nombre) {
+            productos[i].existencia -= 1;
+            if (productos[i].existencia == 0) {
+                visualizarProductos();
+            }
+            sw = false;
+        }
+        guardarAlmacenamientoLocal("productos", productos);
+        i += 1;
+    }
+    numero.innerHTML = lista.length;
+    numero.classList.add("disenoNumero");
+    return lista;
+}
+
+carrito.addEventListener("click", () => {
+    body.style.overflow = "hidden";
+    contenedorCompra.classList.remove('none');
+    contenedorCompra.classList.add('contenedorCompra');
+    infoCompra.classList.add('infoCompra');
+    mostrarElementosLista();
+})
+
+function mostrarElementosLista() {
+        productosCompra.innerHTML = "";
+        valorTotal = 0;
+        for (let i = 0; i < lista.length; i++) {
+            productosCompra.innerHTML += `<div><div class="img"><button onclick=eliminar(${i}) class="botonTrash"><img src="/img/trash.png"></button><p>${lista[i].nombre}</p></div><p> $${lista[i].precio}</p></div>`
+            {/* <div>
+                <div class="img">
+                    <button onclick=eliminar(${i}) class="botonTrash">
+                        <img src="../img/icon_trash.png" alt="Papelera">
+                    </button>
+                    <p>${lista[i].nombre}</p>
+                </div>
+                <p>${lista[i].precio}</p>
+            </div> */}
+            valorTotal += parseInt(lista[i].precio);
+        }
+        total.innerHTML = `<p>Valor Total</p> <p><span>$${valorTotal}</span></p> `
+}
+
+function eliminar(indice){
+    let sw=true;
+    let i=0;
+    while (sw) {
+        if (productos[i].nombre == lista[indice].nombre) {
+            productos[i].existencia += 1;
+            lista.splice(indice,1);
+            sw=false;
+        }
+        i += 1;
+    }
+    guardarAlmacenamientoLocal("productos", productos);
+    numero.innerHTML = lista.length;
+    if (lista.length == 0){
+        numero.classList.remove("disenoNumero")
+    }
+    visualizarProductos();
+    mostrarElementosLista();
+}
+
+x.addEventListener('click', () => {
+    body.style.overflow = "auto";
+    contenedorCompra.classList.add('none');
+    contenedorCompra.classList.remove('contenedorCompra');
+    infoCompra.classList.remove('infoCompra');
+});
